958b5c023583deb7853b560fc09edd1040cc62a9
Kevin Lane <lane.kevin.a@gmail.com>
2019-04-16 21:23:47 -0700

    Add Mandelbrot script

---
+++ mandelbrot.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.animation import FuncAnimation
+
+num = 3000
+max_iter = 50
+
+x = np.linspace(-2, 1, 2*num)
+y = np.linspace(-1, 0, num).reshape(-1, 1)
+c = x + y * 1j
+z = np.zeros(c.shape) + 0j
+in_set = np.ones(c.shape, dtype=bool)
+iterations = np.zeros(c.shape)
+
+fig = plt.figure()
+plt.axis('off')
+img = plt.imshow(np.vstack((iterations, np.flipud(iterations))))
+
+
+def update(i):
+    global in_set
+    z[in_set] = z[in_set] * z[in_set] + c[in_set]
+    in_set = z.real*z.real + z.imag*z.imag <= 4
+    iterations[in_set] += 1
+
+    img.set_data(np.vstack((iterations, np.flipud(iterations))))
+    img.set_clim(0, i)
+
+    return img
+
+
+animation = FuncAnimation(fig, update, frames=max_iter)
+plt.show()
