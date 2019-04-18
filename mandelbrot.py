cdab3ba620603cda2ba62bde64322e3b66189afb
Kevin Lane <lane.kevin.a@gmail.com>
2019-04-17 21:35:03 -0700

    Avoid checking points known to be in the set

--- mandelbrot.py
+++ mandelbrot.py
@@ -10,7 +10,9 @@ x = np.linspace(-2, 1, 2*num)
 y = np.linspace(-1, 0, num).reshape(-1, 1)
 c = x + y * 1j
 z = np.zeros(c.shape) + 0j
-in_set = np.ones(c.shape, dtype=bool)
+q = (x - 0.25) ** 2 + y * y
+check = q * (q + x - 0.25) > 0.25 * y * y
+in_set = np.ones(c.shape, dtype=bool) & check
 iterations = np.zeros(c.shape, dtype=int)
 
 fig = plt.figure()
@@ -21,7 +23,7 @@ img = plt.imshow(np.vstack((iterations, np.flipud(iterations))), cmap='gnuplot2'
 def init():
     global z, in_set, iterations
     z = np.zeros(c.shape) + 0j
-    in_set = np.ones(c.shape, dtype=bool)
+    in_set = np.ones(c.shape, dtype=bool) & check
     iterations = np.zeros(c.shape, dtype=int)
     img.set_data(np.vstack((iterations, np.flipud(iterations))))
 
@@ -31,8 +33,10 @@ def init():
 def update(i):
     global z, in_set, iterations
     z[in_set] = z[in_set] * z[in_set] + c[in_set]
+    zr = z[in_set].real
+    zi = z[in_set].imag
     in_set_prev = np.copy(in_set)
-    in_set = z.real*z.real + z.imag*z.imag <= 4
+    in_set[in_set] = zr * zr + zi * zi <= 4
     iterations[~in_set & in_set_prev] = i+1
 
     img.set_data(np.vstack((iterations, np.flipud(iterations))))
